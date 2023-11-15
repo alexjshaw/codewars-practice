1f18cd065189ae07887cb1632d9f7495337851b7
Alex Shaw <alexjshaw@gmail.com>
2023-11-15 15:55:51 +0000

    space kata

---
+++ 2-Space/Space.js
@@ -0,0 +1,31 @@
+/*
+The Story:
+Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+
+Task Overview:
+You have to write a function that accepts three parameters:
+
+cap is the amount of people the bus can hold excluding the driver.
+on is the number of people on the bus excluding the driver.
+wait is the number of people waiting to get on to the bus excluding the driver.
+If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+*/
+
+// Attempt 1
+
+// function enough(cap, on, wait) {
+//   const space = cap - on
+//   if (wait <= space) {
+//     return 0
+//   } else {
+//     return wait - space
+//   }
+// }
+
+// Attempt 2
+
+function enough(cap, on, wait) {
+  return (on + wait > cap) ? on + wait - cap : 0
+}
+
+module.exports = enough
---
+++ test/spaceTest.js
@@ -0,0 +1,12 @@
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+const enough = require("../2-Space/Space")
+
+describe("Basic Tests", function(){
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(enough(10, 5, 5), 0);
+    assert.strictEqual(enough(100, 60, 50), 10);
+    assert.strictEqual(enough(20, 5, 5), 0);
+  });
+});
